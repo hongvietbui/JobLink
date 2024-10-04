e6700fcd5dab9529d36a773f09ccc32eb2e247a5
Viet Hong Bui <henrygphp@gmail.com>
2024-10-05 04:24:55 +0700

    Added Api request and response DTO

--- JobLink_Backend/DTOs/Request/ApiRequest.cs
+++ JobLink_Backend/DTOs/Request/ApiRequest.cs
@@ -1,6 +1,7 @@
 namespace JobLink_Backend.DTOs.Request;
 
-public class ApiRequest
+public class ApiRequest<T>(T data)
 {
-    
+    public T Data { get; set; } = data;
+    public long Timestamp { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds();
 }
--- JobLink_Backend/DTOs/Response/ApiResponse.cs
+++ JobLink_Backend/DTOs/Response/ApiResponse.cs
@@ -1,6 +1,9 @@
 namespace JobLink_Backend.DTOs.Response;
 
-public class ApiResponse
+public class ApiResponse<T>
 {
-    
+    public int Status { get; set; }
+    public string Message { get; set; }
+    public T? Data { get; set; }
+    public long Timestamp { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds();
 }
