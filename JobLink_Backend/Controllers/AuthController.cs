888b1a5ee1f3bfdf89abfb785853e2c6c9cb39a6
Phuc5438 <hoangphucoh@gmail.com>
2024-10-07 22:21:54 +0700

    create controller

--- JobLink_Backend/Controllers/AuthController.cs
+++ JobLink_Backend/Controllers/AuthController.cs
@@ -1,6 +1,31 @@
+using JobLink_Backend.DTOs.All;
+using JobLink_Backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+
 namespace JobLink_Backend.Controllers;
+[Route("api/[controller]")]
+[ApiController]
 
 public class AuthController : BaseController
 {
-    
+    private readonly IConfiguration _configuration;
+    private readonly JobLinkContext _context;
+
+    public AuthController(IConfiguration configuration, JobLinkContext context)
+    {
+        _configuration = configuration;
+        _context = context;
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] Login user)
+    {
+        var email = _context.Users.FirstOrDefault(x => x.Username == user.Username);
+        var password = _context.Users.FirstOrDefault(x => x.Password == user.Password);
+        if (user != null)
+        {
+            return Ok();
+        }
+        return Unauthorized();
+    }
 }
