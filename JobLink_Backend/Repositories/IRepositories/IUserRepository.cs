5f6afc544f623654cc6544a0ac273725af65bdd4
Cao Viet Hung <caoviethung27102003@gmail.com>
2024-10-08 17:22:43 +0700

    some change for Change password Api

--- JobLink_Backend/Repositories/IRepositories/IUserRepository.cs
+++ JobLink_Backend/Repositories/IRepositories/IUserRepository.cs
@@ -4,5 +4,7 @@ namespace JobLink_Backend.Repositories.IRepositories;
 
 public interface IUserRepository : IRepository<User>
 {
-    
+    User GetById(int userId);
+    void Update(User user);
+    void SaveChanges();
 }
--- JobLink_Backend/Services/ServiceImpls/UserServiceImpl.cs
+++ JobLink_Backend/Services/ServiceImpls/UserServiceImpl.cs
@@ -5,10 +5,11 @@ using JobLink_Backend.Services.IServices;
 
 namespace JobLink_Backend.Services.ServiceImpls;
 
-public class UserServiceImpl(IUnitOfWork unitOfWork) : IUserService
+public class UserServiceImpl(IUnitOfWork unitOfWork, IUserRepository userRepository) : IUserService
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
-    
+    private readonly IUserRepository _userRepository = userRepository;
+
     public async Task SaveRefreshTokenAsync(string username, string refreshToken)
     {
         var user = await _unitOfWork.Repository<User>().FirstOrDefaultAsync(x => x.Username == username);
@@ -20,5 +21,25 @@ public class UserServiceImpl(IUnitOfWork unitOfWork) : IUserService
     {
         var user = await _unitOfWork.Repository<User>().FirstOrDefaultAsync(u => u.Username == username && u.Password == password, u => u.Role);
         return user;
+    }
+
+    public bool ChangePassword(int userId, string currentPassword, string newPassword)
+    {
+        var user = _userRepository.GetById(userId);
+        if(user == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if(user.Password != currentPassword)
+        {
+            throw new Exception("Current password is incorrect");
+        }
+
+        user.Password = newPassword;
+        _userRepository.Update(user);
+        _userRepository.SaveChanges();
+
+        return true;
     }
 }
