3baa5475154de77ebcabb65afcd873a0035e7eee
Phuc5438 <hoangphucoh@gmail.com>
2024-10-17 23:41:56 +0700

    create login

--- JobLink_Front-end/JoblinkFrontEnd/src/App.jsx
+++ JobLink_Front-end/JoblinkFrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/test/test';
 import VerifyEmailPage from './components/forgot-pass/VerifyGmail'
 import ChangePasswordPage from './components/forgot-pass/ChangePassForgot'
+import LoginPage from './components/login/Login';
 function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/verifyEmail" element={<VerifyEmailPage />} />
         <Route path="/changePasswordPage" element={<ChangePasswordPage />} />
+        <Route path="/Login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
