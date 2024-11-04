0a0802b84546c534740aa2c181b2561f4a6cc52d
Viet Hong Bui <henrygphp@gmail.com>
2024-11-04 20:28:33 +0700

    Added notification service

---
+++ JobLink_Backend/Hubs/BaseHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace JobLink_Backend.Hubs;
+
+public class BaseHub : Hub
+{
+    public override Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        if (httpContext == null)
+        {
+            // Log hoặc xử lý lỗi
+            throw new InvalidOperationException("HttpContext is null.");
+        }
+
+        var userId = httpContext.Request.Query["userId"].ToString();
+        if (string.IsNullOrEmpty(userId))
+        {
+            // Log hoặc xử lý lỗi
+            throw new ArgumentException("UserId is missing or invalid.");
+        }
+
+        Groups.AddToGroupAsync(Context.ConnectionId, userId);
+        return base.OnConnectedAsync();
+    }
+}
--- JobLink_Backend/Hubs/ChatHub.cs
+++ JobLink_Backend/Hubs/ChatHub.cs
@@ -2,14 +2,8 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace JobLink_Backend.Hubs;
 
-public class ChatHub : Hub
+public class ChatHub : BaseHub
 {
-    public override Task OnConnectedAsync()
-    {
-        var userId = Context.GetHttpContext().Request.Query["userId"].ToString();
-        Groups.AddToGroupAsync(Context.ConnectionId, userId);
-        return base.OnConnectedAsync();
-    }
     public async Task SendMessage(string user, string message)
     {
         // Gửi tin nhắn cho tất cả người dùng kết nối
