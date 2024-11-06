a3422c0fb4658c8186fbb6dbafa7c93da94a86b3
Nguyễn Vinh Quang <112387320+quangnv1911@users.noreply.github.com>
2024-11-07 01:10:29 +0700

    fix routes

--- JobLink_Front-end/JoblinkFrontEnd/src/routes/Route.jsx
+++ JobLink_Front-end/JoblinkFrontEnd/src/routes/Route.jsx
@@ -2,7 +2,14 @@ import ChangePass from "@/components/change-pass/ChangePass";
 import Dashboard from "@/components/dashboard/Dashboard";
 import ChangePasswordPage from "@/components/forgot-pass/ChangePassForgot";
 import VerifyEmailPage from "@/components/forgot-pass/VerifyGmail";
+import ConfirmJob from "@/components/job/ConfirmationScreen";
+import CreateJob from "@/components/job/CreateJob";
+import CreateLocation from "@/components/job/CreateLocation";
 import LandingPage from "@/components/landing-page/LandingPage";
+import JobList from "@/components/list-job/ListJob";
+import LoginPage from "@/components/login/Login";
+import Home from "@/components/test/test";
+import UserProfile from "@/components/user-profile/UserProfileDetail";
 import MoneyWithdrawal from "@/components/withdraw-money/WithdrawMoney";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const RoutesConfig = () => {
@@ -15,6 +22,12 @@ const RoutesConfig = () => {
         <Route path="/changePasswordPage" element={<ChangePasswordPage />} />
         <Route path="/withdraw-money" element={<MoneyWithdrawal />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/listJob" element={<JobList />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="/createLocation" element={<CreateLocation />} />
+        <Route path="/createJob" element={<CreateJob />} />
+        <Route path="/confirmJob" element={<ConfirmJob />} />
         {/* <Route path="/chat" element={<Chat />} /> */}
       </Routes>
     </Router>
