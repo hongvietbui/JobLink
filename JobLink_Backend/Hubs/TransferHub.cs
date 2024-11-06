6b9a0b09d075d61e1a934cfc85b4342c6164e0c3
Viet Hong Bui <henrygphp@gmail.com>
2024-11-06 23:12:39 +0700

    Update TransferHub.cs

--- JobLink_Backend/Hubs/TransferHub.cs
+++ JobLink_Backend/Hubs/TransferHub.cs
@@ -4,26 +4,6 @@ namespace JobLink_Backend.Hubs;
 
 public class TransferHub : BaseHub
 {
-    public override Task OnConnectedAsync()
-    {
-        var httpContext = Context.GetHttpContext();
-        if (httpContext == null)
-        {
-            // Log hoặc xử lý lỗi
-            throw new InvalidOperationException("HttpContext is null.");
-        }
-
-        var userId = httpContext.Request.Query["userId"].ToString();
-        if (string.IsNullOrEmpty(userId))
-        {
-            // Log hoặc xử lý lỗi
-            throw new ArgumentException("UserId is missing or invalid.");
-        }
-
-        Groups.AddToGroupAsync(Context.ConnectionId, userId);
-        return base.OnConnectedAsync();
-    }
-    
     public async Task SendTransferMessageToUser(Guid userId, string message)
     {
         await Clients.Group(userId.ToString()).SendAsync("ReceiveTransfer", message);
